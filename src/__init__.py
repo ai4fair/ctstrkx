b78b6a1fe0213fb2fe2d66f1f5aee41e7b158900
Adeel Akram <adeel.chep@gmail.com>
2022-09-26 19:09:34 +0200

    CC and Wrangler Folders Added to the SRC/. Post-processing will be done via the SRC/ in future.

--- src/__init__.py
+++ src/__init__.py
@@ -8,6 +8,15 @@ from .reader import SttCSVReader, Draw_Reader_Event
 from .event import Compose_Event, Draw_Compose_Event
 
 
+# From Exa.TrkX NeurIPS19
+import itertools
+def pairwise(iterable):
+  """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
+  a, b = itertools.tee(iterable)
+  next(b, None)
+  return zip(a, b)
+
+
 
 
 
