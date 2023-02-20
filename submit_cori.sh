9cf8970023268260f1075e9c41ce44e36dfa934f
Adeel Akram <adeel.chep@gmail.com>
2023-02-20 13:03:38 +0100

    Proper activation of Conda envs inside (non-interactive) Slurm script.

--- submit_cori.sh
+++ submit_cori.sh
@@ -3,13 +3,28 @@
 # 1 Node, 1 Task, 1 GPU
 
 #SBATCH -A m3443
+#SBATCH -J ctd
 #SBATCH -C gpu
-#SBATCH -q regular
-#SBATCH -t 8:00:00
+#SBATCH -q regular                 # special, early_science
+#SBATCH -t 4:00:00
 #SBATCH -n 1
 #SBATCH --ntasks-per-node=1
-#SBATCH -c 10
+#SBATCH -c 64
 #SBATCH --gpus-per-task=1
+#SBATCH --signal=SIGUSR1@90        # wall signal
+
+# *** I/O ***
+#SBATCH -D .
+#SBATCH -o logs/%x-%j.out
+#SBATCH -e logs/%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=a.akram@gsi.de
+
+mkdir -p logs
+eval "$(conda shell.bash hook)"
+conda activate exatrkx-cori
+export EXATRKX_DATA=$PSCRATCH
 
 export SLURM_CPU_BIND="cores"
 srun traintrack configs/pipeline_fulltrain.yaml
+
